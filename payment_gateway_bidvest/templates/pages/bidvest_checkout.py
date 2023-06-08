0fd1ec2c9bea76fe68f776657f6c59430fd073a1
Nigel <hypej.10@gmail.com>
2023-06-08 14:28:52 +0200

    fix currency issue

--- payment_gateway_bidvest/templates/pages/bidvest_checkout.py
+++ payment_gateway_bidvest/templates/pages/bidvest_checkout.py
@@ -30,6 +30,7 @@ def get_context(context):
 		#'notify_url':context.gateway_details.get('notify_url') or f"{frappe.utils.get_url()}/bidvest_notify",
 		'txndatetime':f"{frappe.utils.now_datetime().strftime('%Y:%m:%d-%H:%M:%S')}",
 		'timezone':f"{frappe.get_doc('System Settings').time_zone}",
+		#'currency': data.get('currency'),
 		#'sharedsecret': context.gateway_details.get('passphrase') or '',
 	}
 	submission_data=build_submission_data(submission_data)
